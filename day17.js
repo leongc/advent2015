6c8552108fa265b52b57d6c56c5fa09f41a8c349
Cheng Leong <cleong@yahoo.com>
2015-12-17 01:03:58 -0600

    Update day17.js
    part b

--- day17.js
+++ day17.js
@@ -33,3 +33,39 @@ function loop(target, capacities) {
 }
 
 console.log(loop(150, capacities));
+
+// part b
+function targetBits(counter, capacities, target) {
+  var sum = 0;
+  var bits = 0;
+  for (var j = 0; j<capacities.length; j++) {
+    if ((1 << j) & counter) {
+      bits++;
+      sum += capacities[j];
+      if (sum > target) {
+        return 0;
+      }
+    }
+  }
+  if (sum === target) {
+    return bits;
+  }
+}
+function loopMin(target, capacities) {
+  var minBits = Infinity;
+  var minCount = 0;
+  var top = 1 << capacities.length;
+  for (var i = 1; i < top; i++) {
+    var bits = targetBits(i, capacities, target);
+    if (bits > 0) {
+      if (bits < minBits) {
+        minBits = bits;
+        minCount = 1;
+      } else if (bits === minBits) {
+        minCount++;
+      }
+    }
+  }
+  return minCount;
+}
+console.log(loopMin(150, capacities));
