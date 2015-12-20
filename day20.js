7139f7a9fb1bc7f91ef645757420e18912975718
Cheng Leong <cleong@yahoo.com>
2015-12-20 04:15:56 -0600

    Update day20.js
    part b

--- day20.js
+++ day20.js
@@ -19,3 +19,24 @@ while (presents(x) < target) {
   x++;
 }
 console.log(x);
+
+// part b
+function presents50(i) {
+  var sum = 0;
+  for (var j=1; j<=Math.sqrt(i); j++) {
+    if (i % j == 0) {
+      if (i <= (50 * j)) {
+        sum += j;
+      }
+      if (i <= (50 * i / j) && Math.pow(j, 2) !== i) {
+        sum += i/j;
+      }
+    }
+  }
+  return 11 * sum;
+}
+var x = 1;
+while (presents50(x) < target) {
+  x++;
+}
+console.log(x);
