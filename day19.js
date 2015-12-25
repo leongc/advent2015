01c63fd8d06f1f1999ef1ed609c61ba351fff641
Cheng Leong <cleong@yahoo.com>
2015-12-25 07:28:06 -0600

    Update day19.js
    filter unused replacements
    use priority queue instead of bfs for steps
    guide search with matchLength
    bound look-back and look-ahead by matchLength progress

--- day19.js
+++ day19.js
@@ -1,6 +1,11 @@
 // problem: http://adventofcode.com/day/19
 // input: http://adventofcode.com/day/19/input
 
+/**
+ * @param s string mapping formatted as 'atom => molecule'
+ * @param replacements input Object multimap from atom to array of molecules
+ * @return replacements with added mapping from s
+ */
 function readReplacementLine(s, replacements) {
   if (!s) { return replacements; }
   var inOut = s.split(' => ');
@@ -40,50 +45,131 @@ function transforms(source, replacements) {
 }
 var inputs = document.body.innerText.split('\n');
 var inputReplacements = {};
+var medicine;
 for (var i = 0; i < inputs.length; i++) {
   var line = inputs[i];
   if (!line) { continue; }
   if (line.indexOf(' => ') >= 0) {
     inputReplacements = readReplacementLine(line, inputReplacements);
   } else {
+    medicine = line;
     console.log(transforms(line, inputReplacements).length);
   }
 }
 
 // part b
-var stepReplacements = {
-  'e': ['H', 'O'],
-  'H': ['HO', 'OH'],
-  'O': ['HH']
-};
-function steps(target, replacements, limit) {
-  var step = 0;
-  var input = ['e'];
-  if (limit === undefined) {
-    limit = 10;
-  }
-  var map = {};
-  while (++step < limit) {
-    var candidates = [];
-    for (var i = 0; i < input.length; i++) {
-      var output = transforms(input[i], replacements);
-      for (var j = 0; j < output.length; j++) {
-        if (output[j] === target) {
-          return step;
-        } else if (output[j].length > target.length) {
-          continue; // transforms stay the same or grow longer, so this is a dead end
-        } else if (map[output[j]] === undefined) {
-          map[output[j]] = step;
-          candidates.push(output[j]);
-        } 
-      }
+testReplacements = readReplacementLine('e => H', testReplacements);
+testReplacements = readReplacementLine('e => O', testReplacements);
+
+/**
+ * @param arr array of strings to be modified
+ * @param maxLen maximum length of string to keep
+ * @return arr without any strings longer than maxLen
+ */
+function filterLength(arr, maxLen) {
+  for (var i = arr.length; i-->0; ) {
+    if (arr[i].length > maxLen) {
+      arr.splice(i, 1); // delete from array
     }
-    input = candidates.slice(0);
   }
-  return map;
+  return arr;
+}
+/**
+ * @param candidate to compare to target
+ * @param target desired match
+ * @return lowest index of candidate element which differs from corresponding target element
+ *         or candidate.length when all candidate elements match corresponding target elements
+ */
+function matchLength(candidate, target) {
+  for (var i = 0; i < candidate.length; i++) {
+    if (candidate[i] !== target[i]) {
+      return i;
+    }
+  }
+  return candidate.length;
+}
+/**
+ * @return comparator function (suitable for use with Array.sort or priority_queue)
+ *         with items assumed to be arrays of size 2, [string, step number]
+ *         which compares first by length of target match (more is better), 
+ *         then by absolute length (shorter is better), 
+ *         then by natural order
+ *         and finally by step number (fewer is better)
+ */
+function targetComparator(target) {
+  return function(a, b) {
+    if (a[0] === b[0]) {
+      if (a[1] === b[1]) {
+        return 0; 
+      }
+      return a[1] - b[1]; // fewer steps are better
+    }
+    var aMatch = matchLength(a[0], target);
+    var bMatch = matchLength(b[0], target);
+    if (aMatch != bMatch) { return bMatch - aMatch; } // more match is better
+    if (a[0].length != b[0].length) { return a[0].length - b[0].length; } // shorter is better
+    return (a[0] < b[0]) ? -1 : 1; // lexicographic order
+  };
+}
+// use priority queue from https://github.com/agnat/js_priority_queue/blob/master/priority_queue.js
+function steps(target, replacements, limit) {
+  limit = limit || 10;
+  var cmp = targetComparator(target);
+  var q = [];
+  var pq = new priority_queue.PriorityQueue(cmp, q);
+  pq.push(['e', 0]);
+  var map = {};
+  var seenit = 0;
+  var next;
+  var bestMatch;
+  var bestMatchLength = 0;
+  var i = 0;
+  while (next = pq.shift()) {
+    if (++i % 100000 == 0) {
+      console.log('Checking: ' + input + '.\tpq size ' + q.length);
+    }
+    var input = next[0];
+    var step = next[1] + 1;
+    if (step > limit) {
+      continue;
+    }
+    var inputMatchLength = matchLength(input, target);
+    if (inputMatchLength < bestMatchLength - 10) { // limit backtracking
+      continue;
+    }
+    var output = transforms(input, replacements);
+    // transforms stay the same or grow longer, therefore excess length is a dead end
+    output = filterLength(output, Math.min(target.length, bestMatchLength + 15));
+    // console.log(input + ' generates: ' + output.join(','));
+    for (var j = 0; j < output.length; j++) {
+      var oj = output[j];
+      if (oj === target) {
+        // console.log(map);
+        return step;
+      }
+      if (step >= map[oj]) {
+        if (++seenit % 1000000 == 0) {
+          console.log('Map saved ' + seenit + ' lookups');
+        }
+        continue;
+      }
+      map[oj] = step;
+      var curMatchLength = matchLength(oj, target);
+      if (curMatchLength > bestMatchLength) {
+        bestMatchLength = curMatchLength;
+        bestMatch = oj;
+        console.log('Best match: ' + oj + '\t' + step);
+      }
+      pq.push([oj, step]);
+    }
+    // TODO: peek at the bottom of the pq
+    // and conditionally remove based on the bestMatchLength
+  }
+  return bestMatch;
 }
 // console.log(steps('HOHOHO', inputReplacements, 10));
 
+/*
 var testReductions = [{'HH':'O'}, {'HO':'H'}, {'OH':'H'}, {'O':'e'}, {'H':'e'}];
 function reduce(input, reductions, reduceMap, limit) {
   if (limit === undefined) {
@@ -171,3 +257,118 @@ inputReductions.sort(function(a, b) {
   return ka.length > kb.length ? -1 : 1;
 });
 // console.log(reduce(inputs[inputs.length-2], inputReductions, 0, {}, 30));
+*/
+function isUpper(c) {
+  return c === c.toUpperCase();
+}
+function atomize(s) {
+  var result = [];
+  if (!s) return result;
+  var atom = s[0];
+  for (var i = 1; i < s.length; i++) {
+    if (isUpper(s[i])) { // new element begins with capital letter
+      result.push(atom);
+      atom = s[i];
+    } else { // continue existing element name
+      atom += s[i];
+    }
+  }
+  return result;
+}
+/** @return true iff all array elements are in the set */
+function allIn(arr, set) {
+  for (var i = 0; i < arr.length; i++) {
+    if (!set.has(arr[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+/** @return new Object with only relevant replacements */
+function filterReplacements(replacements, target) {
+  var result = {};
+  var ras = Object.keys(replacements);
+  var usefulSet = new Set(ras.concat(atomize(target)));
+  for (ra in replacements) {
+    var rms = replacements[ra];
+    for (var j = 0; j < rms.length; j++) {
+      var rm = rms[j];
+      var rmAtoms = [...(new Set(atomize(rm)))]; // dedupe atoms
+      if (allIn(rmAtoms, usefulSet)) {
+        if (result[ra] === undefined) {
+          result[ra] = [];
+        }
+        result[ra].push(rm);
+      }
+    }
+  }
+  return result;
+}
+var filteredReplacements = filterReplacements(inputReplacements, medicine);
+
+
+// use priority queue from https://github.com/agnat/js_priority_queue/blob/master/priority_queue.js
+(function() { // namespace
+
+var exports = (typeof module !== 'undefined' && module.exports) ?
+    module.exports : window.priority_queue = {};
+
+exports.PriorityQueue = function PriorityQueue(compare, queue) {
+  if (!(this instanceof PriorityQueue)) return new PriorityQueue(compare, queue);
+
+  compare = compare || min_first;
+  queue   = queue   || [];
+
+  function swap(i, j) { var t = queue[i]; queue[i] = queue[j]; queue[j] = t; }
+
+  function heapify(i) {
+    var length = queue.length, x, l, r;
+    while (true) {
+      x = i; l = left(i); r = right(i);
+      if (l < length && compare(queue[l], queue[x]) < 0) x = l;
+      if (r < length && compare(queue[r], queue[x]) < 0) x = r;
+      if (x === i) break;
+      swap(i, x);
+      i = x;
+    }
+  }
+
+  function remove(i) {
+    var t = queue[i], b = queue.pop();
+    if (queue.length > 0) {
+      queue[i] = b;
+      heapify(i);
+    }
+    return t;
+  }
+
+  this.push = function push(/* element, ... */) {
+    var i = queue.length, e = i + arguments.length, j, p;
+    queue.push.apply(queue, arguments);
+    for (; i < e; ++i) {
+      j = i; p = parent(i);
+      for (; j > 0 && compare(queue[j], queue[p]) < 0; j = p, p = parent(j)) {
+        swap(j, p);
+      }
+    }
+    return queue.length;
+  }
+
+  this.shift = function shift() { return remove(0); }
+  this.__defineGetter__('length', function length() { return queue.length });
+
+  for (var i = parent(queue.length - 1); i >= 0; --i) { heapify(i) }
+}
+
+function left(i)   { return 2 * i + 1 }
+function right(i)  { return 2 * i + 2 }
+function parent(i) { return Math.floor((i + 1) / 2) - 1 }
+
+var max_first = exports.max_first = function max_first(a, b) { return b - a }
+  , min_first = exports.min_first = function min_first(a, b) { return a - b }
+  ;
+
+})(); // end of namespace 
+
+// Best match: ORnPBPMgArCaCaCaSiThCaCaSiThCaCaPBSiRnFArRnFArCaCaSiThCaCaSiThCaCaCaCaCaCaSiRnFYFArSiRnMgArCaSiRnPTiTiBFYPBFArSiRnCaSiRnTiRnFArSiAlArPTiBPTiRnCaSiAlArCaPTiTiBPMgYFArPTiRnFArSiRnCaCaFYFArF	77
+// console.log(steps(medicine, inputReplacements, 500))
